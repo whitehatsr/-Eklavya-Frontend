247e2128c40d13dfb225e337ad2e449bc1ff1d6a
shiva <shivanedevesh@gmail.com>
2021-11-17 19:49:49 +0530

    Login Activity Added

---
+++ app/src/main/java/com/example/eklavyafrontend/LoginActivity.kt
@@ -0,0 +1,17 @@
+package com.example.eklavyafrontend
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun open_registration(view: android.view.View) {
+    }
+
+    fun redirectIt(view: android.view.View) {}
+}

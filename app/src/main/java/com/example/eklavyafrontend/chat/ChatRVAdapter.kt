83b0c1af6339bfb55e717e6318fe942ab6d995d8
Alok Joshi <AlokJoshi523@gmail.com>
2021-11-21 19:04:53 +0530

    Fixed bugs

--- app/src/main/java/com/example/eklavyafrontend/chat/ChatRVAdapter.kt
+++ app/src/main/java/com/example/eklavyafrontend/chat/ChatRVAdapter.kt
@@ -7,20 +7,18 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.eklavyafrontend.R
-
 class ChatRVAdapter(var userId: String) : RecyclerView.Adapter<ChatViewHolder>() {
     private val items: ArrayList<ChatInfo> = ArrayList()
-    var pos: Int = 0
 
-    fun checkCurr(): Int {
+    /*fun checkCurr(): Int {
         if (items[pos].from == userId) {
             return 1
         }
         return 0
-    }
+    }*/
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatViewHolder {
-        if (checkCurr() == 1) {
+        /*if (checkCurr() == 1) {
             val view = LayoutInflater.from(parent.context)
                 .inflate(R.layout.sent_message_container, parent, false)
             val viewHolder = ChatViewHolder(view)
@@ -30,35 +28,50 @@ class ChatRVAdapter(var userId: String) : RecyclerView.Adapter<ChatViewHolder>()
                 .inflate(R.layout.received_message_container, parent, false)
             val viewHolder = ChatViewHolder(view)
             return viewHolder
-        }
+        }*/
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.sent_message_container, parent, false)
+        val viewHolder = ChatViewHolder(view)
+        return viewHolder
     }
 
     override fun onBindViewHolder(holder: ChatViewHolder, position: Int) {
         val currentItem = items[position]
-        pos = position
+        //pos = position
+
+        holder.messageView.text = currentItem.message
 
 
         if (currentItem.from == userId) {
-            holder.messageView.text = currentItem.message
             holder.messageView.textAlignment = View.TEXT_ALIGNMENT_TEXT_END
             //holder.messageView.textColors = Color.parseColor("#00FF00")
             holder.messageView.setTextColor(Color.parseColor("#00FF00"))
         }
         else {
-            holder.messageView.text = currentItem.message
             holder.messageView.textAlignment = View.TEXT_ALIGNMENT_TEXT_START
             holder.messageView.setTextColor(Color.parseColor("#FF6600"))
         }
         if (currentItem.to == userId) {
-            holder.messageView.text = currentItem.message
             holder.messageView.textAlignment = View.TEXT_ALIGNMENT_TEXT_START
             holder.messageView.setTextColor(Color.parseColor("#FF6600"))
         }
         else {
-            holder.messageView.text = currentItem.message
             holder.messageView.textAlignment = View.TEXT_ALIGNMENT_TEXT_END
             holder.messageView.setTextColor(Color.parseColor("#00FF00"))
         }
+
+        /*if (currentItem.from == userId) {
+            holder.messageView.setTextColor(Color.parseColor("#00FF00"))
+        }
+        else {
+            holder.messageView.setTextColor(Color.parseColor("#FF6600"))
+        }
+        if (currentItem.to == userId) {
+            holder.messageView.setTextColor(Color.parseColor("#FF6600"))
+        }
+        else {
+            holder.messageView.setTextColor(Color.parseColor("#00FF00"))
+        }*/
 
     }
 
